c9c8da00bdd6a008e48050389c173ca279e65d73
rujeenaal-zyoud <rujeenaalzoud@gmail.com>
2021-07-01 15:21:47 -0700

    insert&append

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,11 +1,25 @@
 'use strict';
 
 // Require our linked list implementation
-const LinkedLilst = require('../index');
+const LinkedList = require('../index');
+const Node = require('../node');
+
 
 describe('Linked List', () => {
   it('works', () => {
     expect(true).toBeTruthy();
+  });
+});
+
+
+
+describe('Node Module', () => {
+  it('create an instance with value and next', () => {
+    const value = 'some value';
+    const node = new Node(value);
+    expect(node).toBeInstanceOf(Node);
+    expect(node.value).toEqual(value);
+    expect(node.next).toBeNull();
   });
 });
 
@@ -28,21 +42,29 @@ describe('Linked List', () => {
 });
 
 
-// describe('insert methode to the head', () => {
-//   it('should add node  to empty list first time', () => {
-//     const linkedList = new LinkedList();
-//     linkedList.insert('a');
-//     expect(linkedList.head.value).toEqual('a');
-//     expect(linkedList.head.next).toBeNull();
-//   });
-//   it('should add to a list with values', () => {
-//     const linkedList = new LinkedList();
-//     linkedList.insert('a');
-//     linkedList.insert('b');
-//     expect(linkedList.head.value).toEqual('b');
-//     expect(linkedList.head.next.value).toEqual('a');
-//   });
+describe('append methode to the end of list', () => {
+  it('should append to empty list', () => {
+    const linkedList = new LinkedList();
+    linkedList.append('a');
+    expect(linkedList.head.value).toEqual('a');
+    linkedList.append('a');
+    expect(linkedList.head.value).toEqual('a');
+
+  });
+  it('should append  to a list with values', () => {
+    const linkedList = new LinkedList();
+    linkedList.append('a');
+    linkedList.append('b');
+    linkedList.append('c');
+
+    expect(linkedList.head.value).toEqual('a');
+    expect(linkedList.head.next.value).toEqual('b');
+    expect(linkedList.head.next.next.value).toEqual('c');
+    expect(linkedList.head.next.next.next).toBeNull();
 
 
-// });
+  });
+
+
+});
 
--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,50 @@
 'use strict';
+const Node = require ('./node');
 
 class LinkedList {
-constructor(){}
+constructor()
+{
+    //we put the head here to easy our live :)
+    //becuse node just have next pointer and value in it
+    this.head =null
+}
+
+
+//craete insert function  that add new node in first 
+
+insert(value){
+    //create a node by calling class Node 
+    //here the next will be null becuse we did'nt passing it
+    const node = new Node(value);
+    // if we did'nt have a head it mean it's empatey linked list so pointer the head into new node
+    if(!this.head){
+        this.head =node ;
+    }
+    // if we have actully a node in our linked list so do 
+    else {
+        //will  change the pointer head into new node  and connection
+// connect the new node with first node (that have head pointer)
+        node.next=this.head;
+        //move the pointer head into new node
+        this.head =node
+
+    }
+}
+
+// in draw you under stand this part just
+append(value) {
+    const node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+    } else {
+      let currentNode = this.head;
+      while (currentNode.next) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = node;
+    }
+  }
+
 }
 
 module.exports = LinkedList;
